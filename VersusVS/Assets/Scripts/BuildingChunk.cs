a3e2f3afdaa8622a1e1e72c46510d1b243a479b7
Victor Zaragoza <vzapo99@gmail.com>
2024-02-28 11:12:39 +0100

    Edifcios se rompen o algo parecido no estoy seguro de mi vida que alguien me de trabajo porfavor

---
+++ VersusVS/Assets/Scripts/BuildingChunk.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingChunk : MonoBehaviour
+{
+    [SerializeField] private bool middleChunk = false;
+    [SerializeField] private bool leftChunk = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player" && collision.transform.GetComponent<Player>().vulnerableState)
+        {
+            BuildingLine BL = GetComponentInParent<BuildingLine>();
+
+            if (!middleChunk && !BL.lerping)
+            {                
+                if (leftChunk)
+                {
+                    BL.rightToLeft = false;
+                }
+                else
+                {
+                    BL.rightToLeft = true;
+                }
+                BL.breakThroughBuilding(collision.gameObject);
+                Destroy(this.transform.gameObject);
+            }
+            else
+            {
+                Destroy(this.transform.gameObject);
+            }
+            
+        }
+    }
+}
---
+++ VersusVS/Assets/Scripts/BuildingLine.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingLine : MonoBehaviour
+{
+    [SerializeField] private Transform leftChunk;
+    [SerializeField] private Transform rightChunk;
+
+    [SerializeField] private float endForce = 100;
+
+    private Vector2 leftPos;
+    private Vector2 rightPos;
+
+    private GameObject playerBreakingThrough;
+    public float interpolationDuration = 1;
+
+    private float elapsedTime = 0;
+    private float percentageComplete = 0;
+
+    [HideInInspector] public bool lerping = false;
+    [HideInInspector] public bool rightToLeft = false;
+
+    private void Start()
+    {
+        leftPos = leftChunk.transform.position;
+        rightPos = rightChunk.transform.position;
+    }
+
+    private void Update()
+    {
+        if (lerping)
+        {
+            playerBreakingThrough.GetComponent<Player>().enabled = false;
+            playerBreakingThrough.GetComponent<Player>().vulnerableState = true;
+
+            elapsedTime += Time.deltaTime;
+            percentageComplete = elapsedTime / interpolationDuration;
+
+            if (rightToLeft)
+            {
+                playerBreakingThrough.transform.position = Vector2.Lerp(rightPos, leftPos, percentageComplete);
+            }
+            else
+            {
+                playerBreakingThrough.transform.position = Vector2.Lerp(leftPos, rightPos, percentageComplete);
+            }
+            
+            
+
+            if (percentageComplete >= 1f) {
+
+                endBreackThrough();
+            }
+
+            
+        }
+    }
+
+    public void breakThroughBuilding(GameObject Player)
+    {
+        playerBreakingThrough = Player;
+        lerping = true;
+    }
+
+    public void endBreackThrough()
+    {
+        playerBreakingThrough.GetComponent<Player>().enabled = true;
+        lerping = false;
+
+        Vector2 dir;
+
+        if (rightToLeft) dir = new Vector2(-1, 0);
+        else dir = new Vector2(1, 0);
+
+        playerBreakingThrough.GetComponent<Player>().vulnerableState = false;
+        playerBreakingThrough.GetComponent<Rigidbody2D>().AddForce(dir * endForce, ForceMode2D.Impulse);
+
+    }
+}
